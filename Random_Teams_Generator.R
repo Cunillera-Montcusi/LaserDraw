e8e5bd9ba1bc2eece7746dc32435e1358eb57033
Cunillera-Montcusi <david.cunillera@dcm.cat>
2025-06-19 14:35:48 +0200

    New update with better text and adding Marci

--- Random_Teams_Generator.R
+++ Random_Teams_Generator.R
@@ -1,14 +1,25 @@
-Members<-c("Barbi","Dorina","Huong","Varsha","Bea","Juli","Thinh","Lila","David","Csilla")
+# Team members
+Members<-c("Barbi","Dorina","Huong","Varsha",
+           "Bea","Juli","Thinh","Lila","David","Csilla","Marci")
+# How many teams do you want? 
 number_of_teams<-2
-teams_pos<-sample(1:length(Members),
-                  replace=F,
-                  size=length(Members))
+
+# Random team order
+teams_pos<-sample(1:length(Members),replace=F,size=length(Members))
 #print(teams_pos)
-ini_pos<-c(1,length(Members)/number_of_teams+1)
-fin_pos<-c(length(Members)/number_of_teams,length(Members))
+
+# Positions to select the teams
+ini_pos<-c(1,ceiling(length(Members)/number_of_teams)+1)
+fin_pos<-c(ceiling(length(Members)/number_of_teams),length(Members))
 #print(ini_pos)
 #print(fin_pos)
 
+# Team generator
 for(team in 1:number_of_teams){
-  print(Members[teams_pos[ini_pos[team]:fin_pos[team]]])
+  cat("Team", team,
+      "will be formed by",
+      Members[teams_pos[ini_pos[team]:fin_pos[team]]],
+      "\n",
+      sep=" ")
 }
+
